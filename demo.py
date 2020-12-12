0413dc58a72861b77b38505688642e99ef152686
EvanGertis <physevangertis@gmail.com>
2020-12-12 10:49:01 -0500

    loading courses from csv and user

--- demo.py
+++ demo.py
@@ -4,12 +4,17 @@ import pprint
 semeseter_description = "a code of [S], [SU], [F] and [O] that indicates the semester that the CS department offers the course. And these codes stand for the semester “Spring”, “Summer”, “Fall”, and “all semesters”, respectively."
 input_courses = {}
 input_courses["course"] = {}
-courses = []
+courses = {}
+courses["course"] = {}
+# TODO: transform courses.txt to csv and initialize courses object
 with open('courses.csv', 'r') as csvfile:
     csvreader = csv.DictReader(csvfile, delimiter=',',quotechar='"')
 
     for row in csvreader:
-        print(row)
+        courses["course"]["course_number"] = row["course_number"]
+        courses["course"]["course_title"]  = row["course_title"] 
+        courses["course"]["semester_code"] = row["semester_code"]
+        courses["course"]["prerequisite"]  = row["prerequisite"]
 
 val = ""
 while(val != "q"):
@@ -23,4 +28,5 @@ while(val != "q"):
     if val == "q":
         break
 
-print(input_courses)
+
+print(courses)
