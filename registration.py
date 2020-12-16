068bbd9b22d1bd78f9c5c52083970820ce2bc8c7
EvanGertis <physevangertis@gmail.com>
2020-12-16 06:09:24 -0500

    this shows the deduction

--- registration.py
+++ registration.py
@@ -12,16 +12,32 @@ class registration(Fact):
 
 
 class KE(KnowledgeEngine):
+
+    how = False
+    
     @Rule(
         AND(
-            registration(course_name="CSCI5333", semester='F'),
-            prerequisite(course_name='MATH1441', grade=L('C') | L('B') | L('A')),
-            prerequisite(course_name='MATH2130', grade=L('C') | L('B') | L('A')),
-            prerequisite(course_name='CSCI1301', grade=L('C') | L('B') | L('A'))
+            AS.registration  << registration(course_name="CSCI5333", semester='F'),
+            AS.prerequisite1 << prerequisite(course_name='MATH1441', grade=L('C') | L('B') | L('A')),
+            AS.prerequisite2 << prerequisite(course_name='MATH2130', grade=L('C') | L('B') | L('A')),
+            AS.prerequisite3 << prerequisite(course_name='CSCI1301', grade=L('C') | L('B') | L('A'))
         )
     )
-    def can_register(self):
-        print("registering")
+    def can_register(
+        self, 
+        registration, 
+        prerequisite1,
+        prerequisite2,
+        prerequisite3):
+        if(self.how == True):
+            print("Since semester is ",registration['semester'])
+            print("Since ", prerequisite1['course_name']," was passed with ", prerequisite1['grade'])
+            print("Since ", prerequisite2['course_name']," was passed with ", prerequisite2['grade'])
+            print("Since ", prerequisite3['course_name']," was passed with ", prerequisite3['grade'])
+            print("Registering for: ",registration["course_name"]," during semester: ",registration['semester'])
+        else:
+            print("Registering for: ",registration["course_name"]," during semester: ",registration['semester'])
+        
 
 
 engine = KE()
@@ -34,5 +50,6 @@ engine.declare(
     prerequisite(course_name='CSCI1301', grade='A'),
     registration(course_name=course_no, semester=semester)
     )
+engine.how = True
 print("attempting to register for %s during semester %s " % (course_no, semester))
 engine.run()
